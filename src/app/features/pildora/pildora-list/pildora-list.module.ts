07a06e1aecaf38540fc014bb8b22778a47a289a9
Guillermo David García <davidgarciaguillermo@gmail.com>
2024-02-03 21:38:41 +0100

    update 2

--- src/app/features/pildora/pildora-list/pildora-list.module.ts
+++ src/app/features/pildora/pildora-list/pildora-list.module.ts
@@ -1,21 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { BreadcrumbModule } from 'primeng/breadcrumb';
-import { TableModule } from 'primeng/table';
-import { MenuModule } from 'primeng/menu';
-import { FormsModule } from '@angular/forms';
 import { MessageService } from 'primeng/api';
-import { ToastModule } from 'primeng/toast';
-import { TooltipModule } from 'primeng/tooltip';
-import { PaginatorModule } from 'primeng/paginator';
 import { PildoraListComponent } from './pildora-list.component';
-import { CardModule } from 'primeng/card';
-import { ButtonModule } from 'primeng/button';
-import { MultiSelectModule } from 'primeng/multiselect';
-import { CheckboxModule } from 'primeng/checkbox';
-import { AccordionModule } from 'primeng/accordion';
-import { SelectButtonModule } from 'primeng/selectbutton';
 
 @NgModule({
   declarations: [PildoraListComponent],
@@ -23,19 +10,12 @@ import { SelectButtonModule } from 'primeng/selectbutton';
   imports: [
     CommonModule,
     RouterModule,
-    BreadcrumbModule,
-    TableModule,
-    MenuModule,
-    FormsModule,
-    ToastModule,
-    TooltipModule,
-    PaginatorModule,
-    CardModule,
-    ButtonModule,
-    MultiSelectModule,
-    CheckboxModule,
-    AccordionModule,
-    SelectButtonModule
+    MatSnackBarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatTableModule,
+    MatPaginatorModule,
   ],
   providers: [MessageService],
 })
